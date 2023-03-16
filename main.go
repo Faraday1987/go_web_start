9da169d54a80d53e92fb07cb8d82830219351600
Faraday1987 <faraday1987@gmail.com>
2023-03-16 09:18:51 -0600

    Render HTML pages

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
+
+const PORT = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("This is the home page")
+	renderTemplate(w, "home.page.html")
 }
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("This is the about page")
+	renderTemplate(w, "about.page.html")
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(PORT, nil)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template ", err)
+		return
+	}
 }
